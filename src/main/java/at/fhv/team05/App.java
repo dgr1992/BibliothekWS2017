fb91495e075e4bc091b221be7fab4d7a4e341011
Prokyonn <dma2695@students.fhv.at>
2017-10-22 17:43:14 +0200

    Add files via upload
    maven project

---
+++ src/main/java/at/fhv/team05/App.java
@@ -0,0 +1,13 @@
+package at.fhv.team05;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
