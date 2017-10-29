510a474e43662501f673f1d6a8aaa85dd811f337
Flowkii <florian.braun@students.fhv.at>
2017-10-29 14:08:19 +0100

    Presentation structure added

---
+++ Client/src/main/java/at/fhv/team05/Presentation/Search/SearchPresenter.java
@@ -0,0 +1,7 @@
+package at.fhv.team05.Presentation.Search;
+
+/**
+ * Created by Flo on 29.10.17.
+ */
+public class SearchPresenter {
+}
---
+++ Client/src/main/java/at/fhv/team05/Presentation/Search/SearchView.java
@@ -0,0 +1,9 @@
+package at.fhv.team05.Presentation.Search;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+/**
+ * Created by Flo on 29.10.17.
+ */
+public class SearchView extends FXMLView {
+}
