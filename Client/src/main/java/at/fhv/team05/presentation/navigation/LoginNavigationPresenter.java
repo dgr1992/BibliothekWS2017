6862681cbe551de35f2e0bef1d16dfcb4c254f6d
Michelle <michelle.weber@students.fhv.at>
2017-10-29 16:25:53 +0100

    navigation bar

---
+++ Client/src/main/java/at/fhv/team05/presentation/navigation/LoginNavigationPresenter.java
@@ -0,0 +1,19 @@
+package at.fhv.team05.presentation.navigation;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+/**
+ * Created by Michelle on 29.10.2017.
+ */
+public class LoginNavigationPresenter {
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    void onLoginButtonPressed(ActionEvent event) {
+
+    }
+}
---
+++ Client/src/main/java/at/fhv/team05/presentation/navigation/LoginNavigationView.java
@@ -0,0 +1,9 @@
+package at.fhv.team05.presentation.navigation;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+/**
+ * Created by Michelle on 29.10.2017.
+ */
+public class LoginNavigationView extends FXMLView {
+}
