36deb5784cd4c358b8c6d4455f9a2e8e8431a929
Fabian Reisegger <fabian.reisegger@students.fhv.at>
2017-10-30 16:53:01 +0100

    updated SearchController and RMI Interface

--- Server/src/main/java/at/fhv/team05/RMI/SearchController.java
+++ Server/src/main/java/at/fhv/team05/RMI/SearchController.java
@@ -5,24 +5,29 @@ import at.fhv.team05.dtos.IBook;
 import at.fhv.team05.persistence.BookRepository;
 import at.fhv.team05.rmiinterfaces.SearchForBook;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.HashSet;
 import java.util.LinkedList;
 
-public class SearchController implements SearchForBook {
+public class SearchController extends UnicastRemoteObject implements SearchForBook {
     private BookRepository _bookRepository = null;
     private HashSet<Book> _bookSet;
     private LinkedList<IBook> _foundBooks;
 
     private static SearchController instance;
 
-    public SearchController() {
+    public SearchController() throws RemoteException {
+
+        super();
+
         _bookRepository = new BookRepository();
         _bookSet = new HashSet<>();
 
         _bookSet.addAll(_bookRepository.list());
     }
 
-    public static SearchController getInstance() {
+    public static SearchController getInstance() throws RemoteException {
         if (instance == null) {
             instance = new SearchController();
         }
@@ -30,8 +35,7 @@ public class SearchController implements SearchForBook {
     }
 
     @Override
-    public LinkedList<IBook> searchForBook(String title, String author, String ISBN) {
-        SearchController bookController = SearchController.getInstance();
+    public LinkedList<IBook> searchForBook(String title, String author, String ISBN) throws RemoteException {
 
         _foundBooks = new LinkedList<>();
 
