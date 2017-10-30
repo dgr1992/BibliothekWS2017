fcac4ecd7a31745f2a9706a93cf4e10de7ba8ada
DanielMathis <daniel.mathis@sector8.eu>
2017-10-30 14:23:05 +0100

    dependency cycle temporary fix

--- RMIInterface/src/main/java/at/fhv/team05/rmiinterfaces/SearchForBook.java
+++ RMIInterface/src/main/java/at/fhv/team05/rmiinterfaces/SearchForBook.java
@@ -1,9 +1,8 @@
 package at.fhv.team05.rmiinterfaces;
 
-import at.fhv.team05.domain.Book;
 
 import java.rmi.Remote;
 
-public interface SearchForBook extends Remote {
-    Book searchForBook(String title, String author, String ISBN, String Genre);
+public interface SearchForBook<T> extends Remote {
+    T searchForBook(String title, String author, String ISBN, String Genre);
 }
