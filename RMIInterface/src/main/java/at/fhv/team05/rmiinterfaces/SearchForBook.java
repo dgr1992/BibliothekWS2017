8ee96bb0e48ed813818c92fa37647c7a23c8eda2
Michelle <michelle.weber@students.fhv.at>
2017-10-30 15:23:10 +0100

    search for book return linked list

--- RMIInterface/src/main/java/at/fhv/team05/rmiinterfaces/SearchForBook.java
+++ RMIInterface/src/main/java/at/fhv/team05/rmiinterfaces/SearchForBook.java
@@ -3,7 +3,8 @@ package at.fhv.team05.rmiinterfaces;
 import at.fhv.team05.dtos.IBook;
 
 import java.rmi.Remote;
+import java.util.LinkedList;
 
 public interface SearchForBook<T> extends Remote {
-    IBook searchForBook(String title, String author, String ISBN, String Genre);
+    LinkedList<IBook> searchForBook(String title, String author, String ISBN, String Genre);
 }
