a43b64e580c1a5b05e89ac3e2debb43105736db7
Fabian Reisegger <fabian.reisegger@students.fhv.at>
2017-10-30 13:27:43 +0100

    added SearchController

---
+++ Server/src/main/java/at/fhv/team05/application/rmiinterfaces/SearchForBook.java
@@ -0,0 +1,11 @@
+package at.fhv.team05.application.rmiinterfaces;
+
+import at.fhv.team05.domain.Book;
+
+import java.rmi.Remote;
+
+public interface SearchForBook extends Remote {
+
+    Book searchForBook(String title, String author, String ISBN, String Genre);
+
+}
