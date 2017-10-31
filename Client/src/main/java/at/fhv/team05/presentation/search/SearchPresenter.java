f92c5a29f20e975c8376ab8590a90002e0f1d159
Michelle <michelle.weber@students.fhv.at>
2017-10-31 15:11:53 +0100

    added border to button

--- Client/src/main/java/at/fhv/team05/presentation/search/SearchPresenter.java
+++ Client/src/main/java/at/fhv/team05/presentation/search/SearchPresenter.java
@@ -1,6 +1,7 @@
 package at.fhv.team05.presentation.search;
 
 import at.fhv.team05.dtos.IBook;
+import at.fhv.team05.dtos.IDvd;
 import at.fhv.team05.rmiinterfaces.SearchForBook;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -15,6 +16,7 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -55,25 +57,27 @@ public class SearchPresenter {
     private TextField txtFieldDirector;
 
     @FXML
-    private TextField txtFieldActor;
+    private TextField txtFieldAsin;
 
     @FXML
     private TextField txtFieldGenreDvd;
 
     @FXML
-    private TableView<?> tableViewDvdSearch;
+    private TableView<IDvd> tableViewDvdSearch;
 
     @FXML
-    private TableColumn<?, ?> tblColTitleDvd;
+    private TableColumn<IDvd, String> tblColTitleDvd;
 
     @FXML
-    private TableColumn<?, ?> tblColDirector;
+    private TableColumn<IDvd, String> tblColDirector;
 
     @FXML
-    private TableColumn<?, ?> tblColActor;
+    private TableColumn<IDvd, String> tblColAsin;
 
     @FXML
-    private TableColumn<?, ?> tblColGenreDvd;
+    private TableColumn<IDvd, String> tblColGenreDvd;
+    @FXML
+    private TableColumn<IDvd, Date> tblColReleaseDate;
 
 
     @FXML
@@ -102,14 +106,15 @@ public class SearchPresenter {
         if (getDirector() != null) {
             System.out.println(getDirector());
         }
-        if (getActor() != null) {
-            System.out.println(getActor());
+        if (getAsin() != null) {
+            System.out.println(getAsin());
         }
         if (getDvdGenre() != null) {
             System.out.println(getDvdGenre());
         }
     }
 
+    //Book
     private String getAuthor() {
         return txtFiledAuthor.getText();
     }
@@ -126,6 +131,8 @@ public class SearchPresenter {
         return txtFiledIsbn.getText();
     }
 
+
+    //DVD
     private String getDvdTitle() {
         return txtFieldTitleDvd.getText();
     }
@@ -134,8 +141,8 @@ public class SearchPresenter {
         return txtFieldGenreDvd.getText();
     }
 
-    private String getActor() {
-        return txtFieldActor.getText();
+    private String getAsin() {
+        return txtFieldAsin.getText();
     }
 
     private String getDirector() {
@@ -153,6 +160,16 @@ public class SearchPresenter {
 
 
     }
-    
+
+    private void resultTableDvd(List<IDvd> dvdList) {
+        ObservableList<IDvd> resultData = FXCollections.observableArrayList();
+        resultData.addAll(dvdList);
+        tblColTitleDvd.setCellValueFactory(new PropertyValueFactory<>("title"));
+        tblColDirector.setCellValueFactory(new PropertyValueFactory<>("director"));
+        tblColAsin.setCellValueFactory(new PropertyValueFactory<>("asin"));
+        tblColReleaseDate.setCellValueFactory(new PropertyValueFactory<>("releaseDate"));
+
+        tableViewDvdSearch.setItems(resultData);
+    }
 
 }
