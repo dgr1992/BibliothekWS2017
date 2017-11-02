18e2a8433f1bed85476a572aef5537e11639be5f
DanielMathis <daniel.mathis@sector8.eu>
2017-11-02 12:23:35 +0100

    rmi proxy classcast exception fix

--- RMIInterface/src/main/java/at/fhv/team05/rmiinterfaces/IRMIApplicationController.java
+++ RMIInterface/src/main/java/at/fhv/team05/rmiinterfaces/IRMIApplicationController.java
@@ -3,10 +3,11 @@ package at.fhv.team05.rmiinterfaces;
 import at.fhv.team05.dtos.BookDTO;
 import at.fhv.team05.dtos.DvdDTO;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.LinkedList;
 
-public interface IRMIApplicationController {
+public interface IRMIApplicationController extends Remote {
     LinkedList<BookDTO> searchForBook(String title, String author, String ISBN) throws RemoteException;
 
     LinkedList<DvdDTO> searchForDvd(String title, String director, String asin) throws RemoteException;
