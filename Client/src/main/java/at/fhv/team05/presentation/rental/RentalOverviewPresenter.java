adf0baba9cf1fcd11199f6534f2c9245273adf0b
Flowkii <florian.braun@students.fhv.at>
2017-11-05 18:00:26 +0100

    IMediumDTO added getTitle() RentalOberview fill labels with data

--- Client/src/main/java/at/fhv/team05/presentation/rental/RentalOverviewPresenter.java
+++ Client/src/main/java/at/fhv/team05/presentation/rental/RentalOverviewPresenter.java
@@ -1,10 +1,14 @@
 package at.fhv.team05.presentation.rental;
 
+import at.fhv.team05.dtos.BookDTO;
 import at.fhv.team05.dtos.CustomerDTO;
 import at.fhv.team05.dtos.IMediumDTO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class RentalOverviewPresenter {
     private IMediumDTO medium;
@@ -32,7 +36,7 @@ public class RentalOverviewPresenter {
     private Label lblAboValidUntil;
 
     @FXML
-    private Label lblAboRentedUntil;
+    private Label lblRentedUntil;
 
     @FXML
     void onConfirmButtonPressed(ActionEvent event) {
@@ -43,6 +47,26 @@ public class RentalOverviewPresenter {
     void onExtendAboButtonPressed(ActionEvent event) {
 
     }
+
+    public void initialize() {
+        if(medium!=null) {
+            lblMediumNumber.setText(Integer.toString(medium.getId()));
+            lblTitle.setText(medium.getTitle());
+            lblCustomerName.setText(customer.getFirstName() + customer.getLastName());
+            //lblZipCity.setText(customer.getAddressId());
+            lblCustomerNumber.setText(Integer.toString(customer.getCustomerId()));
+            Calendar c = Calendar.getInstance();
+            c.setTime(customer.getPaymentDate());
+            c.add(Calendar.YEAR, 1);
+            lblAboValidUntil.setText(c.getTime().toString());
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.MONTH,1);
+            lblRentedUntil.setText(calendar.getTime().toString());
+        }
+
+    }
+
+
 
     public void setMedium(IMediumDTO medium) {
         this.medium = medium;
