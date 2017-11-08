15ffde95a084a631f201533d61d3f529f567ed45
DanielMathis <daniel.mathis@sector8.eu>
2017-11-08 12:48:37 +0100

    reference cycle fix

--- RMIInterface/src/main/java/at/fhv/team05/dtos/RentalDTO.java
+++ RMIInterface/src/main/java/at/fhv/team05/dtos/RentalDTO.java
@@ -23,6 +23,7 @@ public class RentalDTO implements Serializable {
         _deadline = rental.getDeadline();
         _extendCounter = rental.getExtendCounter();
     }
+
     public RentalDTO(CopyDTO copy, CustomerDTO customer, Date pickupDate, Date deadline) {
         _copy = copy;
         _customer = customer;
@@ -31,8 +32,18 @@ public class RentalDTO implements Serializable {
         _extendCounter = 0;
     }
 
-    public RentalDTO(){
+    public RentalDTO() {
         _extendCounter = 0;
+    }
+
+    public RentalDTO(IRental rental, CopyDTO copyDTO) {
+        _id = rental.getId();
+        _copy = copyDTO;
+        _customer = new CustomerDTO(rental.getCustomer());
+        _pickupDate = rental.getPickupDate();
+        _returnDate = rental.getReturnDate();
+        _deadline = rental.getDeadline();
+        _extendCounter = rental.getExtendCounter();
     }
 
     public int getId() {
@@ -42,43 +53,52 @@ public class RentalDTO implements Serializable {
     public CopyDTO getCopy() {
         return _copy;
     }
-    public void setCopy(CopyDTO copy){
+
+    public void setCopy(CopyDTO copy) {
         _copy = copy;
     }
 
-    public CustomerDTO getCustomer() { return _customer; }
-    public void setCustomer(CustomerDTO customer){
+    public CustomerDTO getCustomer() {
+        return _customer;
+    }
+
+    public void setCustomer(CustomerDTO customer) {
         _customer = customer;
     }
 
     public Date getPickupDate() {
         return _pickupDate;
     }
-    public void setPickupDate(Date pickupDate){
+
+    public void setPickupDate(Date pickupDate) {
         _pickupDate = pickupDate;
     }
 
     public Date getReturnDate() {
         return _returnDate;
     }
-    public void setReturnDate(Date returnDate){
+
+    public void setReturnDate(Date returnDate) {
         _returnDate = returnDate;
     }
 
     public Date getDeadline() {
         return _deadline;
     }
-    public void setDeadline(Date deadline){
+
+    public void setDeadline(Date deadline) {
         _deadline = deadline;
     }
 
     public int getExtendCounter() {
         return _extendCounter;
     }
-    public void setExtendCounter(int extendCounter){
+
+    public void setExtendCounter(int extendCounter) {
         _extendCounter = extendCounter;
     }
-    public void incrementExtendCounter(){
+
+    public void incrementExtendCounter() {
         _extendCounter++;
     }
 }
