b515efcf10426c50a93c0b3d15bafa4d0db5d69e
dgr1992 <dgr1992@students.fhv.at>
2017-11-08 10:50:16 +0100

    added equals and hashcode

--- RMIInterface/src/main/java/at/fhv/team05/dtos/AddressDTO.java
+++ RMIInterface/src/main/java/at/fhv/team05/dtos/AddressDTO.java
@@ -39,4 +39,31 @@ public class AddressDTO implements Serializable{
     public String getCity() {
         return city;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AddressDTO that = (AddressDTO) o;
+
+        if (id != that.id) return false;
+        if (street != null ? !street.equals(that.street) : that.street != null) return false;
+        if (streetNumber != null ? !streetNumber.equals(that.streetNumber)
+                : that.streetNumber != null) {
+            return false;
+        }
+        if (zip != null ? !zip.equals(that.zip) : that.zip != null) return false;
+        return city != null ? city.equals(that.city) : that.city == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (street != null ? street.hashCode() : 0);
+        result = 31 * result + (streetNumber != null ? streetNumber.hashCode() : 0);
+        result = 31 * result + (zip != null ? zip.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        return result;
+    }
 }
