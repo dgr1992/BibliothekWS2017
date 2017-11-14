f905b64f1019f9144313f9211d9de164b4d94a17
Flowkii <florian.braun@students.fhv.at>
2017-11-14 11:23:00 +0100

    CustomerDetail pressenter fill table

--- Client/src/main/java/at/fhv/team05/presentation/customer/CustomerDetailPresenter.java
+++ Client/src/main/java/at/fhv/team05/presentation/customer/CustomerDetailPresenter.java
@@ -1,19 +1,34 @@
 package at.fhv.team05.presentation.customer;
 
+import at.fhv.team05.ClientRun;
 import at.fhv.team05.dtos.CustomerDTO;
+import at.fhv.team05.dtos.CustomerRentalDTO;
 import at.fhv.team05.dtos.RentalDTO;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import jfxtras.scene.layout.HBox;
 
 import java.net.URL;
+import java.rmi.RemoteException;
 import java.util.Date;
 import java.util.ResourceBundle;
 
 public class CustomerDetailPresenter {
+
+    @FXML
+    private TableView<RentalDTO> tableViewCurrent;
+
+    @FXML
+    private TableView<RentalDTO> tableViewHistory;
 
     @FXML
     private Label lblCustomerNr;
@@ -34,7 +49,7 @@ public class CustomerDetailPresenter {
     private Label lblAboUntil;
 
     @FXML
-    private TableColumn<RentalDTO, Integer> tblColCopyNrCurrent;
+    private TableColumn<RentalDTO, String> tblColCopyNrCurrent;
 
     @FXML
     private TableColumn<RentalDTO, String> tblColTitleCurrent;
@@ -49,7 +64,7 @@ public class CustomerDetailPresenter {
     private TableColumn<RentalDTO, Integer> tblColExtensions;
 
     @FXML
-    private TableColumn<RentalDTO, Integer> tblColCopyNrHistory;
+    private TableColumn<RentalDTO, String> tblColCopyNrHistory;
 
     @FXML
     private TableColumn<RentalDTO, String> tblColTitleHistory;
@@ -70,10 +85,30 @@ public class CustomerDetailPresenter {
         lblStreet.setText(customer.getAddress().getStreet() + " " + customer.getAddress().getStreetNumber());
         lblZipCity.setText(customer.getAddress().getZip() + " / " + customer.getAddress().getCity());
         lblAboUntil.setText(customer.getPaymentDate().toString());
+        try {
+            fillTable(ClientRun.controller.getRentalsFor(customer));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public void fillTable(){
+    public void fillTable(CustomerRentalDTO customerRentalDTO){
+        tblColCopyNrCurrent.setCellValueFactory(param -> new SimpleStringProperty (Integer.toString(param.getValue().getCopy().getCopyNumber())));
+        tblColCopyNrHistory.setCellValueFactory(param -> new SimpleStringProperty(Integer.toString(param.getValue().getCopy().getCopyNumber())));
+        tblColPickUpDateCurrent.setCellValueFactory(new PropertyValueFactory<>("pickupDate"));
+        tblColPickupDateHistory.setCellValueFactory(new PropertyValueFactory<>("pickupDate"));
+        tblColDeadline.setCellValueFactory(new PropertyValueFactory<>("deadline"));
+        tblColExtensions.setCellValueFactory(new PropertyValueFactory<>("extendCount"));
+        tblColReturnDate.setCellValueFactory(new PropertyValueFactory<>("returnDate"));
+        tblColTitleCurrent.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getCopy().getMediaType()));
+        tblColTitleHistory.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getCopy().getMediaType()));
+        ObservableList<RentalDTO> resultDataCurrent = FXCollections.observableArrayList();
+        resultDataCurrent.addAll(customerRentalDTO.getCurrent());
+        tableViewCurrent.setItems(resultDataCurrent);
+        ObservableList<RentalDTO> resultDataHistory = FXCollections.observableArrayList();
+        resultDataHistory.addAll(customerRentalDTO.getHistory());
+        tableViewHistory.setItems(resultDataHistory);
 
     }
 
