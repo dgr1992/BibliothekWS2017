4053ba4e788f895863dd05abd9ea57d388be340c
dgr1992 <dgr1992@students.fhv.at>
2017-11-16 13:04:43 +0100

    deploy

--- Server/src/main/resources/start.sh
+++ Server/src/main/resources/start.sh
@@ -1,4 +1,4 @@
 echo 'kill running application'
-kill -9 $(ps aux | grep -F BibliothekWS2017Server | grep -v -F 'grep' | awk '{ print $2 }')
+kill $(ps aux | grep -F BibliothekWS2017Server | grep -v -F 'grep' | awk '{ print $2 }')
 echo 'start new version'
 nohup java -jar ./Server/target/BibliothekWS2017Server-1.0-SNAPSHOT-jar-with-dependencies.jar &
