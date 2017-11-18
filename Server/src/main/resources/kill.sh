ccadad405ec73411abfeeb9344a1f781d0619faa
grd <daniel@familie-gross.at>
2017-11-18 15:19:35 +0100

    Update kill.sh

--- Server/src/main/resources/kill.sh
+++ Server/src/main/resources/kill.sh
@@ -1,2 +1,2 @@
 echo 'kill running application'
-kill $(ps aux | grep -F BibliothekWS2017Server | grep -v -F 'grep' | awk '{ print $2 }')
+kill $(ps aux | grep -F BibliothekWS2017Server | grep -v -F 'grep' | awk '{ print $2 }') || true
