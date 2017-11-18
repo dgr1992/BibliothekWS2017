bf3d0dbd710566f8807f8ba41a4311ab06cc538d
grd <daniel@familie-gross.at>
2017-11-18 15:04:16 +0100

    Auto deploy

--- Server/src/main/resources/start.sh
+++ Server/src/main/resources/start.sh
@@ -1,4 +1,2 @@
 echo 'kill running application'
 kill $(ps aux | grep -F BibliothekWS2017Server | grep -v -F 'grep' | awk '{ print $2 }')
-echo 'start new version'
-nohup java -jar ./Server/target/BibliothekWS2017Server-1.0-SNAPSHOT-jar-with-dependencies.jar &
