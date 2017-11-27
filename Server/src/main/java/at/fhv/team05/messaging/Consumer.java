8de2bc0408c654d073fcba796f6984d724b517e3
DanielMathis <daniel.mathis@sector8.eu>
2017-11-27 14:24:49 +0100

    consumer/producer

--- Server/src/main/java/at/fhv/team05/messaging/Consumer.java
+++ Server/src/main/java/at/fhv/team05/messaging/Consumer.java
@@ -1,4 +1,48 @@
 package at.fhv.team05.messaging;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
 public class Consumer {
+    public void consume() {
+        try {
+
+            // Create a ConnectionFactory
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+
+            // Create a Connection
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+
+            connection.setExceptionListener(this);
+
+            // Create a Session
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create the destination (Topic or Queue)
+            Destination destination = session.createQueue("TEST.FOO");
+
+            // Create a MessageConsumer from the Session to the Topic or Queue
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            // Wait for a message
+            Message message = consumer.receive(1000);
+
+            if (message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                String text = textMessage.getText();
+                System.out.println("Received: " + text);
+            } else {
+                System.out.println("Received: " + message);
+            }
+
+            consumer.close();
+            session.close();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Caught: " + e);
+            e.printStackTrace();
+        }
+    }
 }
--- Server/src/main/java/at/fhv/team05/messaging/Producer.java
+++ Server/src/main/java/at/fhv/team05/messaging/Producer.java
@@ -1,4 +1,44 @@
 package at.fhv.team05.messaging;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
 public class Producer {
+    public void produce() {
+        try {
+            // Create a ConnectionFactory
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+
+            // Create a Connection
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+
+            // Create a Session
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create the destination (Topic or Queue)
+            Destination destination = session.createQueue("TEST.FOO");
+
+            // Create a MessageProducer from the Session to the Topic or Queue
+            MessageProducer producer = session.createProducer(destination);
+            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+            // Create a messages
+            String text = "Hello world! From: " + Thread.currentThread().getName() + " : " + hashCode();
+            TextMessage message = session.createTextMessage(text);
+
+            // Tell the producer to send the message
+            System.out.println("Sent message: " + message.hashCode() + " : " + Thread.currentThread().getName());
+            producer.send(message);
+
+            // Clean up
+            session.close();
+            connection.close();
+        } catch (
+                Exception e) {
+            System.out.println("Caught: " + e);
+            e.printStackTrace();
+        }
+    }
 }
