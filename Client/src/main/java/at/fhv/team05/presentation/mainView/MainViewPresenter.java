7ad1cc0506bcea91c7e0bae8ea10e3532dfd43a1
Fabian Reisegger <fabian.reisegger@students.fhv.at>
2017-11-28 17:14:28 +0100

    added viewMessage

--- Client/src/main/java/at/fhv/team05/presentation/mainView/MainViewPresenter.java
+++ Client/src/main/java/at/fhv/team05/presentation/mainView/MainViewPresenter.java
@@ -15,6 +15,8 @@ import at.fhv.team05.presentation.login.LoginPresenter;
 import at.fhv.team05.presentation.login.LoginView;
 import at.fhv.team05.presentation.message.CreateMessagePresenter;
 import at.fhv.team05.presentation.message.CreateMessageView;
+import at.fhv.team05.presentation.message.ViewMessagePresenter;
+import at.fhv.team05.presentation.message.ViewMessageView;
 import at.fhv.team05.presentation.navigation.LoginNavigationPresenter;
 import at.fhv.team05.presentation.navigation.LoginNavigationView;
 import at.fhv.team05.presentation.navigation.NavigationPresenter;
@@ -208,11 +210,20 @@ public class MainViewPresenter extends Presenter {
         contentContainer.getChildren().setAll(loginView.getView());
     }
 
+
     public void openCreateMessageView() {
         CreateMessageView messageView = new CreateMessageView();
         CreateMessagePresenter presenter = (CreateMessagePresenter) messageView.getPresenter();
         presenter.setParent(this);
         contentContainer.getChildren().setAll(messageView.getView());
+    }
+
+    public void openViewMessageView() {
+        ViewMessageView viewMessageView = new ViewMessageView();
+        ViewMessagePresenter presenter = (ViewMessagePresenter) viewMessageView.getPresenter();
+        presenter.setParent(this);
+        contentContainer.getChildren().setAll(viewMessageView.getView());
+
     }
 
 }
---
+++ Client/src/main/java/at/fhv/team05/presentation/message/ViewMessagePresenter.java
@@ -0,0 +1,24 @@
+package at.fhv.team05.presentation.message;
+
+import at.fhv.team05.ClientRun;
+import at.fhv.team05.ResultDTO;
+import at.fhv.team05.presentation.Presenter;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+import java.rmi.RemoteException;
+
+public class ViewMessagePresenter extends Presenter{
+
+    @FXML
+    private TextArea textOutputField;
+
+    public void receiveMessage() {
+        try {
+            ResultDTO<String> text = ClientRun.controller.receiveMessage();
+            textOutputField.setText(text.getDto());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+}
