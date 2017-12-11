69bc2f02cac96bb8ed7657cc29b67e21e5996a68
dgr1992 <dgr1992@students.fhv.at>
2017-12-11 17:19:26 +0100

    script to deploy

--- Server/src/main/resources/deploy_EE.sh
+++ Server/src/main/resources/deploy_EE.sh
@@ -1,3 +1,3 @@
 /opt/glassfish5/glassfish/bin/asadmin --user admin --passwordfile /opt/glassfish5/glassfish/bin/password.txt undeploy BibliothekWS2017Server || true
-mv ./target/BibliothekWS2017Server-1.0-SNAPSHOT-jar-with-dependencies.jar ./target/BibliothekWS2017Server.jar'
-/opt/glassfish5/glassfish/bin/asadmin --user admin --passwordfile /opt/glassfish5/glassfish/bin/password.txt deploy ./target/BibliothekWS2017Server.jar
+mv ./Server/target/BibliothekWS2017Server-1.0-SNAPSHOT-jar-with-dependencies.jar ./Server/target/BibliothekWS2017Server.jar'
+/opt/glassfish5/glassfish/bin/asadmin --user admin --passwordfile /opt/glassfish5/glassfish/bin/password.txt deploy ./Server/target/BibliothekWS2017Server.jar
