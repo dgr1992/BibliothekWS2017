0e1657b43af2f2fb1f09f98229b58873ddda0fca
Michelle <michelle.weber@students.fhv.at>
2017-12-16 17:19:26 +0100

    test client methods

--- Server/src/main/java/at/fhv/team05/REST/RESTControllerFacade.java
+++ Server/src/main/java/at/fhv/team05/REST/RESTControllerFacade.java
@@ -2,13 +2,16 @@ package at.fhv.team05.REST;
 
 
 import at.fhv.team05.Utility.JSONUtils;
+import at.fhv.team05.library.ResultDTO;
 import at.fhv.team05.library.ResultListDTO;
+import at.fhv.team05.library.dtos.*;
 import at.fhv.team05.library.dtos.BookDTO;
 import at.fhv.team05.library.dtos.CopyDTO;
 import at.fhv.team05.library.dtos.DvdDTO;
 import at.fhv.team05.library.dtos.IMediumDTO;
 import at.fhv.team05.server.Application.ControllerFacade;
-import at.fhv.team05.server.domain.medium.Book;
+import at.fhv.team05.server.Application.LdapController;
+import at.fhv.team05.server.domain.UserAccount;
 
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -24,9 +27,13 @@ import java.util.Set;
 public class RESTControllerFacade extends Application {
 
     private final ControllerFacade _controllerFacade;
+    private final String _key;
+    private UserAccount _account = null;
+    private static final int KEY_LENGTH = 32;
 
     public RESTControllerFacade() {
         _controllerFacade = ControllerFacade.getInstance();
+        _key = LdapController.getRandomHexString(KEY_LENGTH);
     }
 
     @Override
@@ -79,5 +86,37 @@ public class RESTControllerFacade extends Application {
     @Path("/getAllBooks")
     public String getAllBooks() {
         return _controllerFacade.getAllBooksJSON();
+    }
+
+
+    @POST
+    @Produces("application/json")
+    @Path("/rentMedium")
+    public String rentMedium(String jsonRental) {
+        RentalDTO rentalDTO = (RentalDTO) JSONUtils.JSONToObject(jsonRental, RentalDTO.class);
+        ResultDTO<Boolean> result = _controllerFacade.rentMedium(rentalDTO);
+        return JSONUtils.objectToJSON(result.getDto());
+
+    }
+
+    @POST
+    @Produces("application/json")
+    @Path("/searchCopyByCopyNumber")
+    public String searchCopyByCopyNumber(int copyNumber) {
+        ResultDTO<CopyDTO> resultDTO = _controllerFacade.searchCopyByCopyNumber(copyNumber);
+        return JSONUtils.objectToJSON(resultDTO.getDto());
+    }
+
+    @POST
+    @Produces("application/json")
+    @Path("/authenticateUser")
+    public String authenticateUser(String jsonAccount) {
+        UserAccountDTO accountDTO = (UserAccountDTO) JSONUtils.JSONToObject(jsonAccount, UserAccountDTO.class);
+        ResultDTO<Boolean> tmpDTO = _controllerFacade.authenticateUser(accountDTO, _key);
+        //getDTO gibt true, falls login erfolgreich war, ansonsten false
+        if (tmpDTO.getDto()) {
+            _account = _controllerFacade.getDomainAccount(accountDTO);
+        }
+        return JSONUtils.objectToJSON(tmpDTO.getDto());
     }
 }
