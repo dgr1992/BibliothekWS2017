5c2b0353fc518ba534e01b6a13f2c74ac7b7536f
Fabian Reisegger <fabian.reisegger@students.fhv.at>
2018-01-04 12:37:35 +0100

    updated view to hide detail information if search input changes

--- AngularBibliothek/src/app/mock-books.ts
+++
@@ -1,8 +0,0 @@
-import {Book} from './book';
-
-export const BOOKS: Book[] = [
-  { title: 'whatever', isbn: 'nope', author: 'nein', publisher: '', id: '', releaseDate: '' },
-  { title: 'what', isbn: 'eva', author: 'jawoll', publisher: '', id: '', releaseDate: '' },
-  { title: 'kill', isbn: 'it', author: 'nooin', publisher: '', id: '', releaseDate: '' },
-  { title: 'quattro', isbn: 'ohne', author: 'a3', publisher: '', id: '', releaseDate: '' }
-]
